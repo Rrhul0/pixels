5d52f5ef06f06cb19f275e63b51c91da688ec169
Rahul Raj <rahulraj1417@gmail.com>
2023-07-19 10:51:59 +0530

    added signup page

---
+++ pages/signup.tsx
@@ -0,0 +1,38 @@
+import LoginGoogle from '@/components/LoginGoogle'
+import SignupForm from '@/components/SignupForm'
+import useUser from '@/lib/getUserHook'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+
+export default function SignupPage() {
+	const { user, loading } = useUser()
+	const router = useRouter()
+
+	useEffect(() => {
+		if (user && !loading) router.push('/')
+	}, [loading, router, user])
+
+	if (user)
+		return (
+			<>
+				Already Signed in. Go to <Link href={'/'}>Home</Link>
+			</>
+		)
+
+	return (
+		<div className='grid place-items-center h-screen overflow-hidden bg-slate-300'>
+			<div className='w-80 bg-white rounded-xl p-6 flex flex-col gap-6 '>
+				<LoginGoogle />
+				<SignupForm />
+
+				<div className='flex flex-col items-center'>
+					<span className='text-gray-600'>
+						Already have an account?
+					</span>
+					<Link href='/signin'>Sign in</Link>
+				</div>
+			</div>
+		</div>
+	)
+}
